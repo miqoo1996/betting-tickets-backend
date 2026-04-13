849430c1d18c9bd534cc3935c359b8144c51b979
miqayel <miqoo1996@gmail.com>
2026-04-13 23:25:19 +0400

    Add API routes for further use

---
+++ routes/api.php
@@ -0,0 +1,14 @@
+<?php
+
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/test', function () {
+    return response()->json(['message' => 'API is working', 'status' => 'success']);
+});
+
+Route::get('/user', function (Request $request) {
+    return $request->user();
+})->middleware('auth:sanctum');
+
+// Add more API routes here as needed
