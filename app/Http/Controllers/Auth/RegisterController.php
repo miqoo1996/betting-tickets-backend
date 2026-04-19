321bbdca88e86b77e019cf7e98bb23ddc2d0331a
miqayel <miqoo1996@gmail.com>
2026-04-19 23:04:09 +0400

    fixes

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -7,6 +7,7 @@ use App\Http\Responses\ValidationErrorResponse;
 use App\Models\User;
 use App\Traits\ValidatesWithFriendlyErrors;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Hash;
 
 class RegisterController extends Controller
